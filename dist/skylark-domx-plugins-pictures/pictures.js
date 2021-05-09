1cdbfb4c8a2ebf4d2f33e7b2aa97812ad9493f32
hudaokeji <github@hudaokeji.com>
2021-05-09 23:42:21 +0900

    init commit

---
+++ dist/skylark-domx-plugins-pictures/pictures.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-plugins-pictures - The skylark picture plugin library for dom api extension.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-domx-plugins/skylark-domx-plugins-pictures/
+ * @license MIT
+ */
+define(["skylark-langx/skylark"],function(t){"use strict";return t.attach("domx.plugins.pictures")});
+//# sourceMappingURL=sourcemaps/pictures.js.map
